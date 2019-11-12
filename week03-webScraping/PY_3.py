8b7e05b3c1f23d212845c699607903f87328b983
Aidan O'Connor <aoconnor@cru.ie>
2019-11-12 21:02:21 +0000

    Lab 5.1 and 5.2 completed, word documents with results created

--- week03-webScraping/PY_3.py
+++ week03-webScraping/PY_3.py
@@ -1,4 +1,4 @@
-# Test that you can read a file, we will use the carviewer2.html file that we made last week
+# 3. Test that you can read a file, we will use the carviewer2.html file that we made last week
 # should be up a directory and in the week02 folder ie ("../week02/carviewer2.html")
 
 from bs4 import BeautifulSoup
